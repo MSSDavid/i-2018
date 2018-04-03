9b5a13a50e74b856392330c5e2c7135d4e68f0c0
David Matheus <MSSDavid@users.noreply.github.com>
2018-04-02 21:40:48 -0300

    Add files via upload

---
+++ AS01_magico_2/src/atividade1/ExibirArquivo.java
@@ -0,0 +1,25 @@
+package atividade1;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ *
+ * @author Alunoinf_2
+ */
+public class ExibirArquivo {
+    
+    public static void main (String[] args) throws IOException{
+
+    BufferedReader br = new BufferedReader(new FileReader(args[0]));
+    while(br.ready()){
+       String linha = br.readLine();
+       System.out.println(linha);
+    }
+    br.close();
+    
+    
+}
+}
+
